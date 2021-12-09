602846e20579f0d0b53d95e2a2474e7f2e122d0c
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2021-12-10 01:39:11 +0500

    Added cookies session check and updated code

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from flask import Flask
-from flask import request
+from flask import request, make_response, render_template
+import flask
 
 app = Flask(__name__)
 
@@ -14,7 +15,34 @@ def issue_token():
 def do_stuff():
     _headers = request.headers
     for h in _headers:
-        # it's a tuple
         if h[0].startswith("Authorization"):
-            return "You authenticated with {}".format(h)
-    return "No authentication header was received"
+            if "this_is_a_bearer_token" in h[1]:
+                return "Logged in!", 200
+
+            else:
+                return "Unauthenticated, please login again!", 401
+
+    return "Unauthenticated, please login again!", 401
+
+
+
+# For cookies testing
+@app.route('/cookie')
+def get_cookie():
+    response = make_response()
+    response.set_cookie( "user", "test" )
+    return response
+
+
+@app.route('/')
+def index():
+    _headers = request.headers
+    for h in _headers:
+        if h[0].startswith("Cookie"):
+            if "test" in h[1]:
+                return "Logged in!", 200
+
+            else:
+                return "Unauthenticated, please login again!", 401
+
+    return "Unauthenticated, please login again!", 401
